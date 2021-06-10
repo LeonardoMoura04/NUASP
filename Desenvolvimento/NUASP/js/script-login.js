6ba526631b1d844c9952410e77b20891ec04874f
matteusbonotto <matteusbonotto@gmail.com>
2021-06-10 04:40:03 -0300

    Alterado o js do chat
    Alterado o js do chat

--- Desenvolvimento/NUASP/js/script-login.js
+++ Desenvolvimento/NUASP/js/script-login.js
@@ -1,17 +1,25 @@
-// Objetos do DOM:
-const link = document.querySelector(".exibir");
-const form = document.querySelector(".form");
+// Get the modal
+var modal = document.getElementById("myModal");
 
-// Variável que define se o formulário está, ou não, visível:
-let formIsVisible = false;
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
-// Controla o evento `click` do link:
-link.addEventListener("click", function (event) {
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-// Inverte o estado do formulário:
-formIsVisible = !formIsVisible;
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
 
-// Atualiza o valor da propriedade de acordo com o novo estado:
-form.style.display = (formIsVisible ? "block" : "none");
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
 
-});
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
